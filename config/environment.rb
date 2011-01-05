7e056949c01f7386cdcdbb838f3ada8b60c83c68
danigb <danigb@gmail.com>
2011-01-05 12:42:56 +0100

    Happy 2011

--- config/environment.rb
+++ config/environment.rb
@@ -1,23 +1,23 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   #  config.gem "searchlogic"
-  config.gem "newrelic_rpm"
-  config.gem "authlogic"
-  config.gem "formtastic"
+  config.gem "newrelic_rpm", :version => "2.9.4"
+  config.gem "authlogic", :version => "2.1.2"
+  config.gem "formtastic", :version => '0.9.1'
   config.gem 'will_paginate', :source => 'http://gemcutter.org'
   config.gem 'translator', :sounce => 'http://gemcutter.org'
-  config.gem 'inherited_resources', :source => 'http://gemcutter.org'
-  config.gem 'less'
+  config.gem 'inherited_resources', :source => 'http://gemcutter.org', :version => '0.9.2'
+  config.gem 'less', :version => '1.2.21'
   config.gem 'mini_magick', :source => 'http://gemcutter.org'
 
-  config.load_paths << "#{RAILS_ROOT}/lib"
+  config.autoload_paths << "#{RAILS_ROOT}/lib"
   %w().each do |dir|
     config.load_paths << "#{RAILS_ROOT}/app/#{dir}"
   end
